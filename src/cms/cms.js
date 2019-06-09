84330cd4d931068d18d8fe94ca61f0ba45825000
David Leger <davidleger95@me.com>
2019-06-09 19:34:20 -0300

    add teams

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,18 @@
 import CMS from "netlify-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
-
-import AboutPagePreview from "./preview-templates/AboutPagePreview";
-import NewsPostPreview from "./preview-templates/NewsPostPreview";
-import ProductPagePreview from "./preview-templates/ProductPagePreview";
-import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import { Table, TablePreview } from "./widgets/Table";
+// import AboutPagePreview from "./preview-templates/AboutPagePreview";
+// import NewsPostPreview from "./preview-templates/NewsPostPreview";
+// import ProductPagePreview from "./preview-templates/ProductPagePreview";
+// import IndexPagePreview from "./preview-templates/IndexPagePreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
-CMS.registerPreviewTemplate("index", IndexPagePreview);
-CMS.registerPreviewTemplate("about", AboutPagePreview);
-CMS.registerPreviewTemplate("products", ProductPagePreview);
-CMS.registerPreviewTemplate("news", NewsPostPreview);
+CMS.registerWidget("table", Table, TablePreview);
+
+// CMS.registerPreviewTemplate("index", IndexPagePreview);
+// CMS.registerPreviewTemplate("about", AboutPagePreview);
+// CMS.registerPreviewTemplate("products", ProductPagePreview);
+// CMS.registerPreviewTemplate("news", NewsPostPreview);
---
+++ src/cms/widgets/Table.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+  width: auto;
+  border: 1px solid gray;
+`;
+
+// This is the editing component
+export class Table extends Component {
+  render() {
+    console.log(this.props);
+    console.log(this.props.getEditorComponents());
+    return (
+      <table className={this.props.classNameWrapper}>
+        <thead>
+          <tr>
+            <th>Home</th>
+            <th>Score</th>
+            <th>Away</th>
+            <th>Score</th>
+            <th>Date/Time</th>
+            <th>Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <Input placeholder="Home" />
+            </td>
+            <td>
+              <Input placeholder="Score" type="number" />
+            </td>
+            <td>
+              <Input placeholder="Away" />
+            </td>
+            <td>
+              <Input placeholder="Score" type="number" />
+            </td>
+            <td>
+              <Input placeholder="Date" type="date" />
+            </td>
+            <td>
+              <Input placeholder="Location" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Input placeholder="Home" />
+            </td>
+            <td>
+              <Input placeholder="Score" type="number" />
+            </td>
+            <td>
+              <Input placeholder="Away" />
+            </td>
+            <td>
+              <Input placeholder="Score" type="number" />
+            </td>
+            <td>
+              <Input placeholder="Date" type="date" />
+            </td>
+            <td>
+              <Input placeholder="Location" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Input placeholder="Home" />
+            </td>
+            <td>
+              <Input placeholder="Score" type="number" />
+            </td>
+            <td>
+              <Input placeholder="Away" />
+            </td>
+            <td>
+              <Input placeholder="Score" type="number" />
+            </td>
+            <td>
+              <Input placeholder="Date" type="date" />
+            </td>
+            <td>
+              <Input placeholder="Location" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export class TablePreview extends Component {
+  render() {
+    console.log(this.props);
+    return <div>Table Preview</div>;
+  }
+}
