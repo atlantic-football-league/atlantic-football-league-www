5de8c369122a86d8c640c46195afe6d8a30a158c
David Leger <davidleger95@me.com>
2019-06-20 22:26:55 -0300

    remove description from posts

--- src/templates/news-post.js
+++ src/templates/news-post.js
@@ -9,7 +9,6 @@ import Content, { HTMLContent } from "../components/Content";
 export const NewsPostTemplate = ({
   content,
   contentComponent,
-  description,
   tags,
   title,
   helmet
@@ -25,7 +24,6 @@ export const NewsPostTemplate = ({
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
               {title}
             </h1>
-            <p>{description}</p>
             <PostContent content={content} />
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
@@ -95,7 +93,6 @@ export const pageQuery = graphql`
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         title
-        description
         tags
       }
     }
