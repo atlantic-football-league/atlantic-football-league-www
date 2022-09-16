6e807eff8dfa685c31cd165eebfe113b732799a0
David Leger <david.leger@snyk.io>
2022-09-16 18:01:35 -0300

    feat: remove live stream link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { H2 } from "./styles/Text";
 import Panel from "./Panel";
-import LiveStream from "./LiveStream";
 import Standings from "./Standings";
 import UpcomingGames from "./UpcomingGames";
 
 const Sidebar = () => (
   <aside>
-    <LiveStream />
     <H2>Standings</H2>
     <Panel>
       <Standings />
